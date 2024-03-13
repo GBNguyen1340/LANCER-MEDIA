73cd663f7121f9e5cd9c2ec8f1aec4f65e6e9241
Bao Nguyen SM <bao.nguyen@mediatep.com>
2024-03-13 16:05:27 +0700

    create

---
+++ lancer-media-web/app/data/staticData.js
@@ -0,0 +1,24 @@
+const heroData = [
+    {
+      "id": 1,
+      "title": "Lancer Media là đối tác MCN chính thức với nền tảng Tiktok",
+      "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quos mollitia sed quod consectetur at quam dolore praesentium neque eos assumenda iusto nam laborum laboriosam odio blanditiis possimus accusantium recusandae porro exercitationem itaque",
+      "imageUrl": "https://source.unsplash.com/1600x900?event"
+    },
+    {
+      "id": 2,
+      "title": "Quản lý và nhận booking talent và idol đa nền tảng",
+      "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quos mollitia sed quod consectetur at quam dolore praesentium neque eos assumenda iusto nam laborum laboriosam odio blanditiis possimus accusantium recusandae porro exercitationem itaque",
+      "imageUrl": "https://source.unsplash.com/1600x900?studio"
+    },
+    {
+      "id": 3,
+      "title": "Nhận booking studio và thiết bị livestream game, livestream bán hàng đa nền tảng.",
+      "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quos mollitia sed quod consectetur at quam dolore praesentium neque eos assumenda iusto nam laborum laboriosam odio blanditiis possimus accusantium recusandae porro exercitationem itaque",
+      "imageUrl": "https://source.unsplash.com/1600x900?office"
+    }
+  ]
+
+module.exports = {
+  heroData,
+}
